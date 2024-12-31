33ea4319c87a9ee6e03d0a7e9e1bb5e4959ac312
Aria Shi <ariashi15@gmail.com>
2024-12-30 21:57:20 -0500

    deleted some unused things

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Users from './components/Users';
 import DetailedUserPage from './components/DetailedUserPage'
 import NavBar from './components/NavBar';
 import About from './components/About';
-import { useNavigate } from 'react-router-dom';
 
 function App() {
   return (
